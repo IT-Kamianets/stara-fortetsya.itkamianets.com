776b6262de135ca1503654b173174dc69da2be1a
Oleksandra <luagutko.sasha@gmail.com>
2026-03-13 17:48:54 +0100

    refactor(landing): remove unused code and simplify component structure

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,15 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import {
-	ChangeDetectionStrategy,
-	Component,
-	effect,
-	inject,
-	PLATFORM_ID,
-	signal,
-} from '@angular/core';
-import { marked } from 'marked';
-import { LanguageService } from '../../feature/language/language.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
 	templateUrl: './landing.component.html',
@@ -17,27 +6,4 @@ import { LanguageService } from '../../feature/language/language.service';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingComponent {
-	private _isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
-	private _languageService = inject(LanguageService);
-	private _httpClient = inject(HttpClient);
-
-	html = signal('');
-
-	private _language = '';
-
-	constructor() {
-		if (this._isBrowser) {
-			effect(() => {
-				const language = this._languageService.language();
-
-				if (this._language !== language) {
-					this._language = language;
-
-					this._httpClient
-						.get(`/readme/${language}.md`, { responseType: 'text' })
-						.subscribe((content) => this.html.set(marked.parse(content) as string));
-				}
-			});
-		}
-	}
 }
